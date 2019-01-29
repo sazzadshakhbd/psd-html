18c6b2c621b3da2d8ab9bad933af697d0633a1a0
sazzadshakhbd <sazzadshakhbd@gmail.com>
2019-01-29 22:39:23 +0600

    done class 5

--- js/main.js
+++ js/main.js
@@ -41,6 +41,27 @@
 				
     
     
+  var grid =  $('.grid').isotope({
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      masonry: {
+        // use outer width of grid-sizer for columnWidth
+        columnWidth: '.grid-item'
+      }
+    })
+    
+  // filter items on button click
+    $('.portfolio-menu').on( 'click', 'button', function() {
+      var filterValue = $(this).attr('data-filter');
+      grid.isotope({ filter: filterValue });
+    });
+    
+    //for menu active class
+    $('.portfolio-menu button').on('click', function(event) {
+        $(this).siblings('.active').removeClass('active');
+        $(this).addClass('active');
+        event.preventDefault();
+    });
     
     
 })(jQuery);

596f80fe56031cac569ddac31eb8b74b020d7a09
sazzadshakhbd <sazzadshakhbd@gmail.com>
2019-02-02 02:12:24 +0600

    done class 8

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
     
     "use strict";
     
+    // slider active
     $('.slider-active').slick({
           dots: false,
           arrows: false,
@@ -33,9 +34,41 @@
                 slidesToScroll: 1
               }
             }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
+        ]
+    });
+    
+    // blog active
+    $('.blog-active').slick({
+          dots: true,
+          arrows: false,
+          infinite: false,
+          speed: 300,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
         ]
     });
     

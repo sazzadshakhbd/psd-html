f2626937d9840027bac4ac72bd15bef65331331f
sazzadshakhbd <sazzadshakhbd@gmail.com>
2019-02-06 01:01:13 +0600

    done class 10

--- js/main.js
+++ js/main.js
@@ -107,6 +107,41 @@
         ]
     });
     
+    // brand-active
+    $('.brand-active').slick({
+          dots: false,
+          arrows: false,
+          infinite: false,
+          speed: 300,
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
+    });
+    
     
     
     // Portfolio active

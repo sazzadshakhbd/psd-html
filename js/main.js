c077bf3ce39154e6740cd5e271ef18512c8c1e90
sazzadshakhbd <sazzadshakhbd@gmail.com>
2019-02-05 22:38:10 +0600

    done class 9

--- js/main.js
+++ js/main.js
@@ -72,6 +72,41 @@
         ]
     });
     
+    // testimonial-active
+    $('.testimonial-active').slick({
+          dots: true,
+          arrows: false,
+          infinite: false,
+          speed: 300,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
+    });
+    
     
     
     // Portfolio active
